33c60a476e39819af5801b76aa2f23bfa1566c90
Jarrod Wooden <C16jarrod.wooden@usafa.edu>
2014-11-11 13:25:40 -0700

    A Functionality, Etch-a-sketch with remote

---
+++ Lab5A_Func/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+ASM_SRCS += \
+../nokia.asm 
+
+C_SRCS += \
+../main.c 
+
+OBJS += \
+./main.obj \
+./nokia.obj 
+
+ASM_DEPS += \
+./nokia.pp 
+
+C_DEPS += \
+./main.pp 
+
+C_DEPS__QUOTED += \
+"main.pp" 
+
+OBJS__QUOTED += \
+"main.obj" \
+"nokia.obj" 
+
+ASM_DEPS__QUOTED += \
+"nokia.pp" 
+
+C_SRCS__QUOTED += \
+"../main.c" 
+
+ASM_SRCS__QUOTED += \
+"../nokia.asm" 
+
+
